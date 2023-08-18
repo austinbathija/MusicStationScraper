27b79db1e24e6ec0ff47ba5e92b20085483336d3
aubathija <aubathija@gmail.com>
2023-08-17 21:08:23 -0400

    6.0

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
 
+# Function to retrieve song history from the database
 def get_song_history():
     conn = sqlite3.connect('MultiStation_SongData.db')
     cursor = conn.cursor()
@@ -11,14 +12,13 @@ def get_song_history():
     conn.close()
     return songs
 
+# Landing page route
 @app.route('/')
-def index():
-    return render_template('landing.html')
-
 @app.route('/landingpage')
 def landing_page():
     return render_template('landing.html')
 
+# Page routes for each station
 @app.route('/bpm')
 def bpm():
     return render_template('bpm.html')
@@ -31,70 +31,48 @@ def chill():
 def armin():
     return render_template('armin.html')
 
-@app.route('/refresh/bpm')
-def refresh_bpm_song_data():
+# Function to refresh song data for a specific station
+def refresh_song_data(station_name):
     songs = get_song_history()
-
-    bpm_songs = [song for song in songs if song[1] == "BPM"]
+    station_songs = [song for song in songs if song[1] == station_name]
 
     now_playing = ""
     song_history_rows = ""
-    if bpm_songs:
-        latest_song = bpm_songs[-1]  # Get the latest song
-        now_playing = f"<div class='currently-playing'>{latest_song[2]} - {latest_song[3]}</div>"
 
-        # Exclude the latest song from the history
-        history_songs = bpm_songs[:-1]
+    if station_songs:
+        latest_song = station_songs[-1]
+        now_playing = (
+            f"<div class='currently-playing'>"
+            f"<span id='song-title'>{latest_song[2]}</span><br>"
+            f"<span id='artist'>{latest_song[3]}</span></div>"
+        )
+
+        history_songs = station_songs[:-1]
         for song in reversed(history_songs):
-            song_history_rows += f"<tr><td>{song[2]}</td><td>{song[3]}</td><td>{song[4]}</td><td>{song[5]}</td></tr>"
+            song_history_rows += (
+                f"<tr><td>{song[2]}</td><td>{song[3]}</td>"
+                f"<td>{song[4]}</td><td>{song[5]}</td></tr>"
+            )
     else:
-        song_history_rows = "<tr><td colspan='3'>No song history available for BPM station.</td></tr>"
+        song_history_rows = (
+            f"<tr><td colspan='3'>No song history available for "
+            f"{station_name} station.</td></tr>"
+        )
 
     return now_playing + "<!--SPLIT-->" + song_history_rows
 
+# Refresh routes for each station
+@app.route('/refresh/bpm')
+def refresh_bpm():
+    return refresh_song_data("BPM")
 
 @app.route('/refresh/chill')
-def refresh_chill_song_data():
-    songs = get_song_history()
-
-    chill_songs = [song for song in songs if song[1] == "Chill"]
-
-    now_playing = ""
-    song_history_rows = ""
-    if chill_songs:
-        latest_song = chill_songs[-1]  # Get the latest song
-        now_playing = f"<div class='currently-playing'><span id='song-title'>{latest_song[2]}</span><br><span id='artist'>{latest_song[3]}</span></div>"
-
-        # Exclude the latest song from the history
-        history_songs = chill_songs[:-1]
-        for song in reversed(history_songs):
-            song_history_rows += f"<tr><td>{song[2]}</td><td>{song[3]}</td><td>{song[4]}</td><td>{song[5]}</td></tr>"
-    else:
-        song_history_rows = "<tr><td colspan='3'>No song history available for Chill station.</td></tr>"
-
-    return now_playing + "<!--SPLIT-->" + song_history_rows
-
+def refresh_chill():
+    return refresh_song_data("Chill")
 
 @app.route('/refresh/armin')
-def refresh_armin_song_data():
-    songs = get_song_history()
-
-    armin_songs = [song for song in songs if song[1] == "A State of Armin"]
-
-    now_playing = ""
-    song_history_rows = ""
-    if armin_songs:
-        latest_song = armin_songs[-1]  # Get the latest song
-        now_playing = f"<div class='currently-playing'>{latest_song[2]} - {latest_song[3]}</div>"
-
-        # Exclude the latest song from the history
-        history_songs = armin_songs[:-1]
-        for song in reversed(history_songs):
-            song_history_rows += f"<tr><td>{song[2]}</td><td>{song[3]}</td><td>{song[4]}</td><td>{song[5]}</td></tr>"
-    else:
-        song_history_rows = "<tr><td colspan='3'>No song history available for Armin station.</td></tr>"
-
-    return now_playing + "<!--SPLIT-->" + song_history_rows
+def refresh_armin():
+    return refresh_song_data("A State of Armin")
 
 if __name__ == '__main__':
     app.run(debug=True)
